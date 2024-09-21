b9269baa83c7fd42def42a3c94427748b9befd9a
piyushpatil4270 <piyushpatil4270@gmail.com>
2024-09-21 20:19:54 +0530

    Improved the product card

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const ProductCard = ({ product, handleIncrease, quantity, handleDecrease }) => {
  console.log("Quantity of product is ",quantity)
+ console.log("Product is ",product)
   return (
-    <div className='border h-fit p-4 flex flex-col gap-4 rounded-lg shadow-lg bg-white'>
-      <span className='text-[12px] font-semibold'>{product.name}</span>
+    <div className='border h-fit p-4 flex flex-col gap-[4px] rounded-lg shadow-lg bg-white'>
+  <div className='w-full flex flex-col gap-[2px]'>
+  <span className='text-[14px] font-semibold'>{product.name}</span>
+  <div className='flex gap-2 py-2'>
+  <span className='text-[10px] font-semibold'>Target  {product.requiredQty}</span>
+  <span className='text-[10px] font-semibold'>Quantity  {product.currentQty}</span>
+ 
+  </div>
+   </div>
       <div className='flex justify-between items-center'>
         <span className='text-[10px] font-semibold'>${product.amount.toFixed(2)}/unit</span>
         <div className='flex items-center gap-2 rounded-sm border-slate-400 border-[1px] outline-none'>
